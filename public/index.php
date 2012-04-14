f8d14d4942b5fe5a188523c9ff434ed7de2f40bc
Thomas ZILLIOX <thomas@zilliox.me>
2012-04-14 04:57:37 +0200

    Initialization of the repository

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once( dirname( dirname( __FILE__ ) ) . '/classes/Request.class.php' );
+
+$request = new Request( );
+
+print_r( request );
+
+?>
---
+++ utils/Request.class.php
@@ -0,0 +1,54 @@
+<?php
+
+class Request {
+
+
+	/*************************************************************************
+	  ATTRIBUTES                 
+	 *************************************************************************/
+	public $domain;
+	public $path;
+
+
+	/*************************************************************************
+	  CONSTRUCTOR                   
+	 *************************************************************************/
+	public function __construct( ) {
+		$this->init_path( );
+		$this->init_url( );
+	}
+
+
+	/*************************************************************************
+	  PUBLIC METHODS                   
+	 *************************************************************************/
+	public function path( ) {
+		return $this->path;
+	}
+	public function domain( ) {
+		return $this->domain;
+	}
+	public function url( ) {
+		return $this->domain . $this->path;
+	}
+
+
+	/*************************************************************************
+	  PRIVATE METHODS                   
+	 *************************************************************************/
+	private function init_path( ) {
+		$path = urldecode( $_SERVER[ 'REQUEST_URI' ] );
+		if ( String_Utils::contains( $path, '?' ) ) {
+			$path = String_Utils::substr_before( $path, '?' );
+		}
+		if ( ! String_Utils::contains( $path, '/' ) ) {
+			$path .= '/';
+		}
+		$this->path = $path;
+	}
+	private function init_url( ) {
+		$this->domain = $_SERVER[ 'SERVER_NAME' ];
+	}
+}
+
+?>
