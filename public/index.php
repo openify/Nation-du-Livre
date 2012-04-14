c6cc74079571a56754d7af20fa7a3ac70d2c4af9
Thomas ZILLIOX <thomas@zilliox.me>
2012-04-14 05:01:17 +0200

    Initialization du Router

--- public/index.php
+++ public/index.php
@@ -1,9 +1,9 @@
 <?php
 
-require_once( dirname( dirname( __FILE__ ) ) . '/classes/Request.class.php' );
+require_once( dirname( dirname( __FILE__ ) ) . '/utils/Request.class.php' );
 
 $request = new Request( );
 
-print_r( request );
+print_r( $request );
 
 ?>
--- utils/Request.class.php
+++ utils/Request.class.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once( dirname( __FILE__ ) . '/String_Utils.class.php' );
+
 class Request {
 
 
