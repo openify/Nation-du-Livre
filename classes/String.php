328465a64fede92f1dcf4795f2cf2bf8b0741373
Thomas ZILLIOX <thomas@zilliox.me>
2012-04-14 05:20:28 +0200

    Use underscore to seprate word in methods name

--- classes/String.php
+++ classes/String.php
@@ -6,22 +6,22 @@ abstract class String {
 	/*************************************************************************
 	  STATIC METHODS                   
 	 *************************************************************************/
-	static function startswith( $hay, $needle ) {
+	static function starts_with( $hay, $needle ) {
 		return substr( $hay, 0, strlen( $needle ) ) == $needle;
 	}
-	static function endswith( $hay, $needle ) {
+	static function ends_with( $hay, $needle ) {
 		return substr( $hay, -strlen( $needle ) ) == $needle;
 	}
-	static function istartswith( $hay, $needle ) {
+	static function i_starts_with( $hay, $needle ) {
 		return startswith( strtolower( $hay ), strtolower( $needle ) );
 	}
-	static function iendswith( $hay, $needle ) {
+	static function i_ends_with( $hay, $needle ) {
 		return endswith( strtolower( $hay ), strtolower( $needle ) );
 	}
 	static function contains( $hay, $needle ) {
 		return ( strpos( $hay, $needle ) !== false );
 	}
-	static function icontains( $hay, $needle ) {
+	static function i_contains( $hay, $needle ) {
 		return contains( strtolower( $hay ), strtolower( $needle ) );
 	}
 	static function substr_before( $hay, $needle ) {
@@ -30,25 +30,25 @@ abstract class String {
 	static function substr_after( $hay, $needle ) {
 		return substr( $hay, strpos( $hay, $needle ) + 1 );
 	}
-	static function must_startswith( $hay, $needle ) {
+	static function must_starts_with( $hay, $needle ) {
 		if ( ! startswith( $hay, $needle ) ) {
 			$hay = $needle . $hay;
 		}
 		return $hay;
 	}
-	static function must_endswith( $hay, $needle ) {
+	static function must_ends_with( $hay, $needle ) {
 		if ( ! endswith( $hay, $needle ) ) {
 			$hay .= $needle;
 		}
 		return $hay;
 	}
-	static function must_not_startswith( $hay, $needle ) {
+	static function must_not_starts_with( $hay, $needle ) {
 		if ( startswith( $hay, $needle ) ) {
 			$hay = substr( $hay, 1 );
 		}
 		return $hay;
 	}
-	static function must_not_endswith( $hay, $needle ) {
+	static function must_not_ends_with( $hay, $needle ) {
 		if ( endswith( $hay, $needle ) ) {
 			$hay = substr( $hay, 0, -1 );
 		}
