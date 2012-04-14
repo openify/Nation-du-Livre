182d16739066bafd8f46f8e02927ff16a8dde9e7
Thomas ZILLIOX <thomas@zilliox.me>
2012-04-15 00:33:35 +0200

    Error_Controlelr use DefaultView

--- controllers/Error_Controller.php
+++ controllers/Error_Controller.php
@@ -7,8 +7,9 @@ class Error_Controller {
 	  ACTION METHODS                   
 	 *************************************************************************/
         public function view( $error_code, $message ) {
-		echo '<h1>Error ' . $error_code . '</h1>';
-		echo '<p>' . $message . '</p>';
-		echo '<p>Essaye <a href="http://local.nation/book/read/1" >cette url</a></p>';
+		$content =  '<p>' . $message . '</p>';
+		$content .= '<p>Essaye <a href="http://local.nation/book/read/1" >cette url</a></p>';
+		$view = new Default_View( );
+		return $view->render( 'Error ' . $error_code, $content ); 
 	}
 }
