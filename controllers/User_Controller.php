c805bfb48c815e3e384d7bcd8e60288509b0f053
Thomas ZILLIOX <thomas@zilliox.me>
2012-04-15 08:24:13 +0200

    Improve login page

--- controllers/User_Controller.php
+++ controllers/User_Controller.php
@@ -4,22 +4,35 @@ class User_Controller {
 
 
 	/*************************************************************************
-	  ACTION METHODS                   init_by_login
+	  ACTION METHODS                   
 	 *************************************************************************/
 	public function login( ) {
+		$error = '';
 		if ( isset( $_POST['login'] ) ) {
 			$user = new User_Model();
-			$user->init_by_login( $_POST['login'] );
-			if ( $user->checkpass( $user->password, $_POST['password'] ) ) {
-				$content = 'Logged in.';
-			} else {
-				$content = '';
+			try {
+				$user->init_by_login( $_POST['login'] );
+				try {
+					$user->checkpass( $user->password, $_POST['password'] );
+					$title = '';
+					$content = 'Logged in.';
+					$_SESSION[ 'user' ] = $user;
+				} catch( Exception $e ) {
+					// TODO: Compter le nombre d'erreur de login
+					$error = 'Mot de passe incorrect';
+				}
+			} catch( Exception $e ) {
+				$error = 'Login inconnu';
 			}
-			$title = '';
-		} else {
+		}
+		if ( ! isset( $_SESSION[ 'user' ] ) ) {
 			$title = 'Login page';
-			$content = '<form class="form" method="post" action="">' .
-					   '<div><label for="login">Login:</label><input type="text" id="login" name="login" /></div>' .
+			$content = '';
+			if ( ! empty( $error ) ) {
+				$content .= '<p class="warning">' . $error . '</p>'; 
+			}
+			$content .= '<form class="form" method="post" action="">' .
+					   '<div><label for="login">Login:</label><input type="text" id="login" name="login" value="' . ( isset( $_POST['login'] ) ? $_POST['login'] : '' ) . '"/></div>' .
 					   '<div><label for="password">Password:</label><input type="password" id="password" name="password" /></div>' .
 					   '<input type="submit" value="Submit" />' .
 					   '</form>';
--- public/index.php
+++ public/index.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 
 require_once( dirname( dirname( __FILE__ ) ) . '/include.php' );
 
