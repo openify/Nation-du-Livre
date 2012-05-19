ad7de139d94b1d3705ef598f5c7883d3b7191cab
Thomas ZILLIOX <thomas@zilliox.me>
2012-05-19 17:40:58 +0200

    Correction to use the base controller

--- Nation/controller/Welcome.php
+++ Nation/controller/Welcome.php
@@ -2,7 +2,7 @@
 
 namespace Nation\Controller;
 
-class Welcome extends \Controller_Defined {
+class Welcome extends \Controller\__Base {
 
 
 	/*************************************************************************
@@ -17,6 +17,6 @@ class Welcome extends \Controller_Defined {
 	public function view( ) {
 		$this->view->title   = 'Bienvenue';
 		$this->view->content = 'The nation to be';
-		return $this->render( \View::LAYOUT_TEMPLATE ); 
+		return $this->render( \View\__Base::LAYOUT_TEMPLATE ); 
 	}
 }
